3b313261d323b9ca9abe3855c24f0e2bcfcc29b8
seth0027 <sethi.shivam0624@gmail.com>
2021-04-27 18:14:50 -0500

    share news

--- components/ArticleRow.tsx
+++ components/ArticleRow.tsx
@@ -1,13 +1,25 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { View, Image, Text, TouchableHighlight } from "react-native";
+import { View, Image, Text, TouchableHighlight, Share } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { Article } from "../models/NewsResponse";
+import { AntDesign } from "@expo/vector-icons";
 
 type ArticleRowProps = {
   item: Article;
 };
 export const ArticleRow = ({ item }: ArticleRowProps) => {
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: item.url ?? "",
+        url: item.url ?? "",
+        title: item.title,
+      });
+    } catch (err) {
+      alert(err?.message);
+    }
+  };
   const navigation = useNavigation();
   return (
     <View
@@ -20,52 +32,56 @@ export const ArticleRow = ({ item }: ArticleRowProps) => {
         backgroundColor: "white",
       }}
     >
-      <TouchableOpacity
-        onPress={() => {
-          console.log(item.url);
-          navigation.navigate("Details", {
-            url: item.url,
-          });
-        }}
-      >
-        <View>
-          <Image
-            source={{ uri: item.urlToImage }}
-            style={{
-              width: "100%",
-              height: 200,
-              resizeMode: "cover",
-              borderTopLeftRadius: 10,
-              borderTopRightRadius: 10,
+      <View>
+        <Image
+          source={{ uri: item.urlToImage }}
+          style={{
+            width: "100%",
+            height: 200,
+            resizeMode: "cover",
+            borderTopLeftRadius: 10,
+            borderTopRightRadius: 10,
+          }}
+        />
+        <View style={{ margin: 10 }}>
+          <TouchableOpacity
+            onPress={() => {
+              console.log(item.url);
+              navigation.navigate("Details", {
+                url: item.url,
+              });
             }}
-          />
-          <View style={{ margin: 10 }}>
+          >
             <Text style={{ fontSize: 22, fontWeight: "700" }}>
               {item.title}
             </Text>
             <Text style={{ fontSize: 18 }}>{item.description}</Text>
-            <View
-              style={{
-                marginTop: 4,
-                flexDirection: "row",
-                justifyContent: "space-between",
-                flexWrap: "wrap",
-              }}
-            >
-              <Text
-                style={{ fontSize: 16, fontStyle: "italic", color: "grey" }}
-              >
-                {item.author}
-              </Text>
-              <Text
-                style={{ fontSize: 16, fontStyle: "italic", color: "grey" }}
-              >
-                {item.publishedAt && new Date(item.publishedAt).toDateString()}
-              </Text>
-            </View>
+          </TouchableOpacity>
+          <View
+            style={{
+              marginTop: 4,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+            }}
+          >
+            <Text style={{ fontSize: 16, fontStyle: "italic", color: "grey" }}>
+              {item.author}
+            </Text>
+            <Text style={{ fontSize: 16, fontStyle: "italic", color: "grey" }}>
+              {item.publishedAt && new Date(item.publishedAt).toDateString()}
+            </Text>
+            <TouchableOpacity onPress={onShare}>
+              <AntDesign
+                name="sharealt"
+                size={22}
+                color="dimgray"
+                style={{ alignSelf: "center" }}
+              />
+            </TouchableOpacity>
           </View>
         </View>
-      </TouchableOpacity>
+      </View>
     </View>
   );
 };
